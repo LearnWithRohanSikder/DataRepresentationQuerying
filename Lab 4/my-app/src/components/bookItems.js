db68be02616471a6bcd587a1bb20b8e535ca7ac3
Rohan Sikder <Rohansikder4@gmail.com>
2022-10-21 20:40:03 +0100

    Lab 4 Complete

---
+++ Lab 4/my-app/src/components/bookItems.js
@@ -0,0 +1,20 @@
+import React from "react";
+//Card BootStrap Import
+import Card from 'react-bootstrap/Card';
+
+export class BookItems extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Card>
+                    <Card.Body>
+                        {/*Using props of books to display individual Books in the React Bootstrap Card*/}
+                        <Card.Title>{this.props.books.title}</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">{this.props.books.authors}</Card.Subtitle>
+                        <Card.Text><img src={this.props.books.thumbnailUrl}></img></Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+        );
+    }
+}
---
+++ Lab 4/my-app/src/components/create.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+export class Create extends React.Component {
+
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.onChangeBookTitle = this.onChangeBookTitle.bind(this);
+        this.onChangeCover = this.onChangeCover.bind(this);
+        this.onChangeAuthor = this.onChangeAuthor.bind(this);
+        this.state = {
+            title: '',
+            cover: '',
+            author: ''
+        }
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(`${this.state.title}, ${this.state.cover} , ${this.state.author}`);
+        this.setState({
+            title: '',
+            cover: '',
+            author: ''
+        })
+    }
+
+    onChangeBookTitle(e) {
+        this.setState({
+            title: e.target.value
+        })
+    }
+
+    onChangeCover(e) {
+        this.setState({
+            cover: e.target.value
+        })
+    }
+
+    onChangeAuthor(e) {
+        this.setState({
+            author: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h3> Hello from create component </h3>
+                {/*React Forms*/}
+                <form onSubmit={this.handleSubmit}>
+
+                    <div className="form-group">
+                        <label>Add Book Title: </label>
+                        <input type="text" className="form-control" value={this.state.title} onChange={this.onChangeBookTitle} />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Add Book Cover: </label>
+                        <input type="text" className="form-control" value={this.state.cover} onChange={this.onChangeCover} />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Add Author: </label>
+                        <input type="text" className="form-control" value={this.state.author} onChange={this.onChangeAuthor} />
+                    </div>
+
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        );
+    }
+}
